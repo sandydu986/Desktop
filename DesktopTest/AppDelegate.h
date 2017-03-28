6a507c558f7ce07880d644b03bb162606adeeda7
sandydu986 <sandydu986@gmail.com>
2017-03-28 11:00:41 +0800

    DesktopTestDesktopTest.xcodeprojDesktopTestTestsDesktopTestUITests

---
+++ DesktopTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesktopTest
+//
+//  Created by Tiger on 3/28/17.
+//  Copyright © 2017 Tiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
